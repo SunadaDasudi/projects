294082c3fc28d37abc8c65acc1c42bcf09b4a9ee
Sunada Dasudi <sunada.dasudi2010@gmail.com>
2023-04-23 11:52:38 +0200

    Add html-css-javascript project folder

---
+++ html-css-javascript/calander/script.js
@@ -0,0 +1,124 @@
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+const newEventModel = document.getElementById('newEventModal');
+const deleteEventModel = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
+const eventText = document.getElementById('eventText');
+const calendar = document.getElementById('calendar');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+
+function openModal(date) {
+    clicked = date;
+    const eventForDay = events.find(e => e.date == clicked);
+    if (eventForDay) {
+        eventText.innerText = eventForDay.title;
+        deleteEventModel.style.display = 'block';
+    }
+    else {
+        newEventModel.style.display = 'block';
+    }
+    backDrop.style.display = 'block';
+}
+
+function load() {
+    const dt = new Date();
+    if (nav != 0) {
+        dt.setMonth(new Date().getMonth() + nav);
+    }
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear();
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+    document.getElementById('monthDisplay').innerText = `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+
+    calendar.innerHTML = '';
+    for (let i = 1; i < paddingDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+        if (i > paddingDays) {
+            daySquare.innerText = i - paddingDays;
+
+            const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+            let eventForDay = events.find(e => e.date === dayString);
+            if (eventForDay) {
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
+        }
+        else {
+            daySquare.classList.add('padding');
+        }
+
+        calendar.appendChild(daySquare);
+    }
+}
+
+function saveEvent() {
+    if (eventTitleInput.value) {
+        eventTitleInput.classList.remove('error');
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value
+        });
+
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+    }
+    else {
+        eventTitleInput.classList.add('error');
+    }
+}
+
+function closeModal() {
+    newEventModel.style.display = 'none';
+    deleteEventModel.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.classList.remove('error');
+    eventTitleInput.value = '';
+    clicked = null;
+
+    load();
+}
+
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
+}
+
+function initButtons() {
+    document.getElementById('nextButton').addEventListener('click', () => {
+        nav++;
+        load();
+    })
+
+    document.getElementById('backButton').addEventListener('click', () => {
+        nav--;
+        load();
+    })
+    document.getElementById('saveButton').addEventListener('click', saveEvent);
+    document.getElementById('cancelButton').addEventListener('click', closeModal);
+
+    document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+    document.getElementById('closeButton').addEventListener('click', closeModal);
+}
+
+initButtons();
+load();

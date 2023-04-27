fb3d22aeea4991f78b28564a9297a2ee6e5a7f4b
= <=>
2023-04-27 11:27:02 +0200

    Update look & feel

--- trial2/index.js
+++ trial2/index.js
@@ -1,14 +1,73 @@
+
+const side_menu = document.querySelectorAll('.side-menu>img');
+const header_menu = document.getElementById('header-menu');
+const nav_links = document.querySelectorAll('header>nav>ul>li>a');
 (function () {
-    const nav_links = document.querySelectorAll('header>nav>ul>li>a');
+    side_menu[0].addEventListener('click', () => {
+        header_menu.classList.toggle('show');
+    })
+
     nav_links.forEach(nav => {
-        console.log(nav.getAttribute('href').replace('#', ''));
         const sectionId = nav.getAttribute('href').replace('#', '');
-        const section = document.querySelector(nav.getAttribute('href'));
-        const link = document.getElementById(sectionId);
-        console.log(link);
-        link.addEventListener('click', function (event) {
-            event.preventDefault(); // prevent the default link behavior
-            section.scrollIntoView({ behavior: 'smooth' }); // scroll to the section
+        const section = document.getElementById(sectionId);
+        nav.addEventListener('click', function (event) {
+            event.preventDefault();
+            section.scrollIntoView({ behavior: 'smooth' });
+            header_menu.classList.toggle('show');
+            ScrollReveal().reveal('.header-menu', slideDown);
         });
     })
 })();
+
+// ScrollReveal().reveal('#home>p');
+
+var slideRight = {
+    distance: '20%',
+    origin: 'left',
+    opacity: null
+};
+
+var slideLeft = {
+    distance: '40%',
+    origin: 'right',
+    opacity: null
+}
+
+var slideDown = {
+    distance: '50%',
+    origin: 'top',
+    opacity: null
+}
+
+var slideUp = {
+    distance: '50%',
+    origin: 'bottom',
+    opacity: null
+}
+
+ScrollReveal().reveal('.header-menu', slideDown);
+ScrollReveal().reveal('.intro', slideRight);
+ScrollReveal().reveal('.image-container', slideLeft);
+ScrollReveal().reveal('#about-me .two-col .description', slideLeft);
+ScrollReveal().reveal('#portfolio .container', slideLeft);
+ScrollReveal().reveal('.skill-set', slideDown);
+ScrollReveal().reveal('#skills .card', slideUp);
+ScrollReveal().reveal('#contact', slideRight);
+
+function downloadFile() {
+    var link = document.createElement('a');
+    var filename = './trial2/sunada_dasudi_resume.docx';
+    link.href = './';
+    link.download = filename;
+
+    console.log(link);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+const download_resume = document.getElementById('download-resume');
+download_resume.addEventListener('click', downloadFile)
+
+
+

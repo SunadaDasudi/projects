18aa243c8c96f1e26123f621e0c7e4cd494b6331
= <=>
2023-04-27 19:01:14 +0200

    change theme

--- trial2/index.js
+++ trial2/index.js
@@ -3,6 +3,7 @@ const side_menu = document.querySelectorAll('.side-menu>img');
 const header_menu = document.getElementById('header-menu');
 const nav_links = document.querySelectorAll('header>nav>ul>li>a');
 (function () {
+
     side_menu[0].addEventListener('click', () => {
         header_menu.classList.toggle('show');
     })
@@ -19,55 +20,65 @@ const nav_links = document.querySelectorAll('header>nav>ul>li>a');
     })
 })();
 
-// ScrollReveal().reveal('#home>p');
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', sendContactFrom);
+}
+
+function sendContactFrom(event) {
+    emailjs.init('eaQ24HoGrtEEJKPkr');
+    event.preventDefault();
+
+    var templateParams = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phoneNumber: document.getElementById('phonenumber').value,
+        message: document.getElementById('message').value
+    };
+    let service_id = 'service_irh46al';
+    let template_id = 'template_ohlgw3k';
+    emailjs.send(service_id, template_id, templateParams)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
+}
+
 
 var slideRight = {
-    distance: '20%',
+    distance: '10%',
     origin: 'left',
     opacity: null
 };
 
 var slideLeft = {
-    distance: '40%',
+    distance: '10%',
     origin: 'right',
     opacity: null
 }
 
 var slideDown = {
-    distance: '50%',
+    distance: '10%',
     origin: 'top',
     opacity: null
 }
 
 var slideUp = {
-    distance: '50%',
+    distance: '10%',
     origin: 'bottom',
     opacity: null
 }
 
 ScrollReveal().reveal('.header-menu', slideDown);
 ScrollReveal().reveal('.intro', slideRight);
-ScrollReveal().reveal('.image-container', slideLeft);
 ScrollReveal().reveal('#about-me .two-col .description', slideLeft);
+ScrollReveal().reveal('.employment-history', slideUp);
+
 ScrollReveal().reveal('#portfolio .container', slideLeft);
 ScrollReveal().reveal('.skill-set', slideDown);
 ScrollReveal().reveal('#skills .card', slideUp);
-ScrollReveal().reveal('#contact', slideRight);
 
-function downloadFile() {
-    var link = document.createElement('a');
-    var filename = './trial2/sunada_dasudi_resume.docx';
-    link.href = './';
-    link.download = filename;
 
-    console.log(link);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
-
-const download_resume = document.getElementById('download-resume');
-download_resume.addEventListener('click', downloadFile)
 
 
 

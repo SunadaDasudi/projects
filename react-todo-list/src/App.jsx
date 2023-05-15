67ff2aae4ddf9962d3f960afd09f4ba25f838783
sunada-dasudi <sunada.dasudi@avensia.com>
2023-05-15 09:25:19 +0200

    Add files

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./styles.css"
+import NewToDoForm from "./NewToDoForm"
+import ToDoList from "./ToDoList"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  function addTodo(newItem) {
+
+    setTodos(currentTodos => {
+      return [...currentTodos, { id: 1, title: newItem, completed: false }];
+    });
+  }
+
+
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id)
+          return { ...todo, completed }
+        return todo;
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id);
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NewToDoForm onSubmit={addTodo}></NewToDoForm>
+      <ToDoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}>
+      </ToDoList>
     </>
-  )
+  );
 }
-
-export default App
